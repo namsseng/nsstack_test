3149b258982932b130829c4d4d7d29c3ebaac04c
Richerd Namsseng <namsseng@nate.com>
2014-10-02 15:46:31 +0900

    saf
    sfaafsd

--- nsstack_keystone.sh
+++ nsstack_keystone.sh
@@ -5,6 +5,9 @@ if [ "$(id -u)" != "0" ]; then
    echo "You need to be 'root' dude." 1>&2
    exit 1
 fi
+
+
+
 . ./nsstack_setuprc
 
 # get keystone
@@ -1432,27 +1435,27 @@ keystone user-role-add --user=demo --role=_member_ --tenant=demo
 
 # keystone 
 keystone service-create --name=keystone --type=identity --description="OpenStack Identity"
-keystone endpoint-create --service-id=$(keystone service-list | awk '/ identity / {print $2}') --publicurl=http://"$mangementip":5000/v2.0 --internalurl=http://"$mangementip":5000/v2.0 --adminurl=http://"$mangementip":35357/v2.0
+keystone endpoint-create --service-id=$(keystone service-list | awk '/ identity / {print $2}') --publicurl=http://"$managementip":5000/v2.0 --internalurl=http://"$managementip":5000/v2.0 --adminurl=http://"$managementip":35357/v2.0
 
 
 # glance
 keystone user-create --name=glance --pass="$password" --email="$email"
 keystone user-role-add --user=glance --tenant=service --role=admin
 keystone service-create --name=glance --type=image --description="OpenStack Image Service"
-keystone endpoint-create --service-id=$(keystone service-list | awk '/ image / {print $2}') --publicurl=http://"$mangementip":9292 --internalurl=http://"$mangementip":9292 --adminurl=http://"$mangementip":9292
+keystone endpoint-create --service-id=$(keystone service-list | awk '/ image / {print $2}') --publicurl=http://"$managementip":9292 --internalurl=http://"$managementip":9292 --adminurl=http://"$managementip":9292
 
 
 # nova
 keystone user-create --name=nova --pass="$password" --email="$email"
 keystone user-role-add --tenant=service --user=nova --role=admin
 keystone service-create --name=nova --type=compute --description="OpenStack Compute"
-keystone endpoint-create --service-id=$(keystone service-list | awk '/ compute / {print $2}') --publicurl=http://"$mangementip":8774/v2/%\(tenant_id\)s --internalurl=http://"$mangementip":8774/v2/%\(tenant_id\)s --adminurl=http://"$mangementip":8774/v2/%\(tenant_id\)s
+keystone endpoint-create --service-id=$(keystone service-list | awk '/ compute / {print $2}') --publicurl=http://"$managementip":8774/v2/%\(tenant_id\)s --internalurl=http://"$managementip":8774/v2/%\(tenant_id\)s --adminurl=http://"$managementip":8774/v2/%\(tenant_id\)s
 
 # neutron
 keystone user-create --name=neutron --pass="$password" --email="$email"
 keystone user-role-add --tenant=service --user=neutron --role=admin
 keystone service-create --name neutron --type network --description "OpenStack Networking"
-keystone endpoint-create --service-id=$(keystone service-list | awk '/ network / {print $2}') --publicurl=http://"$mangementip":9696 --adminurl=http://"$mangementip":9696 --internalurl=http://"$mangementip":9696
+keystone endpoint-create --service-id=$(keystone service-list | awk '/ network / {print $2}') --publicurl=http://"$managementip":9696 --adminurl=http://"$managementip":9696 --internalurl=http://"$managementip":9696
 
 # cinder
 #keystone user-create --name=cinder --pass="$SERVICE_PASSWORD" --email=$email
